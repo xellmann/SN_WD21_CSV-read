6e23829e10c6f4d07e817e5c2e97f575f5a92f91
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-14 19:51:58 +0200

    code shortend

--- csv_einlesen.py
+++ csv_einlesen.py
@@ -1,9 +1,5 @@
 with open("data.csv", "r") as csv_read:
     lines = csv_read.read().splitlines()
-    print(lines)
     for i in lines:
         a = i.split(",")
-        name = a[0]
-        age = a[1]
-        gender = a[2]
-        print(name + " is " + age + " years old and " + gender)
+        print("{0} is {1} years old and {2}".format(a[0], a[1], a[2]))
