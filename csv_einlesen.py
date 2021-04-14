3d6180af8891b468b1b33ec06ef6de91d085f3a0
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-14 19:40:11 +0200

    printing two dimensional array added

--- csv_einlesen.py
+++ csv_einlesen.py
@@ -2,14 +2,8 @@ with open("data.csv", "r") as csv_read:
     lines = csv_read.read().splitlines()
     print(lines)
     for i in lines:
-        a = str(i.split(","))
+        a = i.split(",")
         name = a[0]
         age = a[1]
         gender = a[2]
-        print(a)
-        #for element in a:
-            #print(element)
-            #print("{0} is {1} years old and {2}".format(element, element, element))
-        #for e in i:
-        #    print(e)
-        #print("{0} is {1} years old and {2}".format(i, i, i))
+        print(name + " is " + age + " years old and " + gender)
